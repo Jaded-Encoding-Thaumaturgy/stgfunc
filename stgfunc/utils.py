06b7431ac5ae49b4eeab80dfbddffeefd0bcd949
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2021-10-22 19:11:22 +0200

    Add mask blur in replace_squaremask

--- stgfunc/utils.py
+++ stgfunc/utils.py
@@ -2,21 +2,30 @@ import lvsfunc as lvf
 import vapoursynth as vs
 from lvsfunc.types import Range
 from vsutil import depth as vdepth
-from typing import Tuple, Union, List, Sequence
+from typing import Tuple, Union, List, Sequence, Optional
 
 
-def replace_squaremask(clipa: vs.VideoNode, clipb: vs.VideoNode, mask_params: Tuple[int, int, int, int], ranges: Union[Range, List[Range], None]) -> vs.VideoNode:
+def replace_squaremask(
+    clipa: vs.VideoNode, clipb: vs.VideoNode, mask_params: Tuple[int, int, int, int],
+    ranges: Union[Range, List[Range], None],
+    blur_sigma: Optional[int] = None
+) -> vs.VideoNode:
   import kagefunc as kgf
+
+  mask = kgf.squaremask(clipb, *mask_params)
+
+  if blur_sigma is not None:
+    mask = mask.bilateral.Gaussian(blur_sigma)
 
   return lvf.rfs(
       clipa, clipa.std.MaskedMerge(
-          clipb, kgf.squaremask(clipb, *mask_params)
+          clipb, mask
       ), ranges
   )
 
 
 def depth(*clips_depth: vs.VideoNode) -> Sequence[vs.VideoNode]:
-  assert type(clips_depth[-1]) is int
+  assert isinstance(clips_depth[-1], int)
 
   clips = [vdepth(clip, clips_depth[-1]) for clip in clips_depth[:-1]]
 
