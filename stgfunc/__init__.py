9547d2ca0ff60ecdb4b615a157cf8fefd4376a86
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-01-11 15:59:23 +0100

    Update __init__.py

--- stgfunc/__init__.py
+++ stgfunc/__init__.py
@@ -1,6 +1,5 @@
-from . import (avghist, denoise, fades, helpers, mask, misc, other, oyster, traiNNing, upscale, utils)  # noqa
+from . import (fades, helpers, mask, misc, other, oyster, traiNNing, upscale, utils)  # noqa
 
 src = misc.source
 depth = utils.depth
 output = misc.set_output
-Maximum = other.Maximum
