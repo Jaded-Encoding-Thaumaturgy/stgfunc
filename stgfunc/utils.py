00c051cb14000472f8c6825b2921b03e9ba25262
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-01-11 14:51:13 +0100

    Add `freeze_replace_mask`, `get_bits to `utils`

--- stgfunc/utils.py
+++ stgfunc/utils.py
@@ -1,7 +1,7 @@
 import lvsfunc as lvf
 import vapoursynth as vs
 from lvsfunc.types import Range
-from vsutil import depth as vdepth
+from vsutil import depth as vdepth, get_depth
 from typing import Tuple, Union, List, Sequence, Optional, Dict, Any
 
 
@@ -25,9 +25,21 @@ def replace_squaremask(
   return lvf.rfs(clipa, merge, ranges) if ranges else merge
 
 
+def freeze_replace_mask(
+    mask: vs.VideoNode, insert: vs.VideoNode,
+    mask_params: Tuple[int, int, int, int], frame: int, frame_range: Tuple[int, int]
+) -> vs.VideoNode:
+  masked_insert = replace_squaremask(mask[frame], insert[frame], mask_params)
+  return lvf.rfs(mask, masked_insert * mask.num_frames, frame_range)
+
+
 def depth(*clips_depth: vs.VideoNode, **kwargs: Dict[str, Any]) -> Sequence[vs.VideoNode]:
   assert isinstance(clips_depth[-1], int)
 
   clips = [vdepth(clip, clips_depth[-1], **kwargs) for clip in clips_depth[:-1]]
 
   return clips[0] if len(clips) == 1 else clips
+
+
+def get_bits(clip: vs.VideoNode, expected_depth: int = 16) -> Tuple[int, vs.VideoNode]:
+  return (bits := get_depth(clip)), vdepth(clip, expected_depth) if bits != expected_depth else clip
