9ca3bdd20decbb7a293f49a45d56d1c44eca48f1
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-02-22 18:21:25 +0100

    Update disallow_* decorators and add overloads

--- stgfunc/types.py
+++ stgfunc/types.py
@@ -3,11 +3,11 @@ from __future__ import annotations
 import vapoursynth as vs
 from abc import abstractmethod
 from functools import partial, wraps
-from typing import runtime_checkable, Protocol, List, Union, Any, TypeVar, Callable, cast
+from typing import runtime_checkable, Protocol, List, Union, Any, TypeVar, Callable, cast, overload
 
+F = TypeVar('F', bound=Callable[..., Any])
 T = TypeVar('T')
 R = TypeVar('R')
-F = TypeVar('F', bound=Callable)
 
 SingleOrArr = Union[T, List[T]]
 SingleOrArrOpt = Union[SingleOrArr[T], None]
@@ -21,7 +21,17 @@ class SupportsString(Protocol):
         pass
 
 
-def disallow_variable_format(function: F, /, *, only_first: bool = False) -> F:
+@overload
+def disallow_variable_format(*, only_first: bool = False) -> Callable[[F], F]:
+    ...
+
+
+@overload
+def disallow_variable_format(function: F | None = None, /) -> F:
+    ...
+
+
+def disallow_variable_format(function: F | None = None, /, *, only_first: bool = False) -> Callable[[F], F] | F:
     """Function decorator that raises an exception if input clips have variable format.
         :param function:    Function to wrap.
         :param only_first:  Whether to check only the first argument or not.
@@ -30,10 +40,11 @@ def disallow_variable_format(function: F, /, *, only_first: bool = False) -> F:
     """
 
     if function is None:
-        return partial(disallow_variable_format, only_first=only_first)
+        return cast(Callable[[F], F], partial(disallow_variable_format, only_first=only_first))
 
     @wraps(function)
     def _check(*args: Any, **kwargs: Any) -> Any:
+        assert function
         if (only_first and args[0].format is None) or any([
             a.format is None for a in args if isinstance(a, vs.VideoNode)
         ]):
@@ -44,7 +55,17 @@ def disallow_variable_format(function: F, /, *, only_first: bool = False) -> F:
     return cast(F, _check)
 
 
-def disallow_variable_resolution(function: F, only_first: bool = False) -> F:
+@overload
+def disallow_variable_resolution(*, only_first: bool = False) -> Callable[[F], F]:
+    ...
+
+
+@overload
+def disallow_variable_resolution(function: F | None = None, /) -> F:
+    ...
+
+
+def disallow_variable_resolution(function: F | None = None, /, *, only_first: bool = False) -> Callable[[F], F] | F:
     """Function decorator that raises an exception if input clips have variable resolution.
         :param function:    Function to wrap.
         :param only_first:  Whether to check only the first argument or not.
@@ -52,8 +73,12 @@ def disallow_variable_resolution(function: F, only_first: bool = False) -> F:
         :return:  Wrapped function.
     """
 
+    if function is None:
+        return cast(Callable[[F], F], partial(disallow_variable_resolution, only_first=only_first))
+
     @wraps(function)
     def _check(*args: Any, **kwargs: Any) -> Any:
+        assert function
         if ((only_first and not all([args[0].width, args[0].height])) or not all([
             c for s in [[a.width, a.height] for a in args if isinstance(a, vs.VideoNode)] for c in s
         ])):
