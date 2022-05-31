d555cde872eea6629d7fd3a22048e757790066a5
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-05-31 22:26:10 +0200

    Update __init__.py imports/exports

--- stgfunc/__init__.py
+++ stgfunc/__init__.py
@@ -1,5 +1,17 @@
-from . import (deband, easing, mask, misc, other, oyster, traiNNing, transitions, tweaking, types, utils)  # noqa
+from . import (  # noqa: F401
+    deband, easing, exprfuncs, mask, misc, noise, other, oyster, traiNNing, transitions, tweaking, types, utils
+)
 
-src = misc.source
-depth = utils.depth
-output = misc.set_output
+from .deband import *  # noqa: F401, F403
+from .easing import *  # noqa: F401, F403
+from .exprfuncs import *  # noqa: F401, F403
+from .mask import *  # noqa: F401, F403
+from .misc import *  # noqa: F401, F403
+from .noise import *  # noqa: F401, F403
+from .other import *  # noqa: F401, F403
+from .oyster import *  # noqa: F401, F403
+from .traiNNing import *  # noqa: F401, F403
+from .transitions import *  # noqa: F401, F403
+from .tweaking import *  # noqa: F401, F403
+from .types import *  # noqa: F401, F403
+from .utils import *  # noqa: F401, F403
