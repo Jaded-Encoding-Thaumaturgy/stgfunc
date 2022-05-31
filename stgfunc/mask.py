3ff7bae1c68df8764ceb3ccc7461a72a2bec5d1b
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-05-31 22:25:03 +0200

    Move mask functions from utils to mask submodule

--- stgfunc/mask.py
+++ stgfunc/mask.py
@@ -146,3 +146,57 @@ def detail_mask(
     rm_grain = core.rgvs.RemoveGrain(merged, rg_mode)
 
     return rm_grain if bits == 16 else depth(rm_grain, bits)
+
+
+@disallow_variable_format
+def squaremask(clip: vs.VideoNode, width: int, height: int, offset_x: int, offset_y: int) -> vs.VideoNode:
+    assert clip.format
+
+    bits = get_depth(clip)
+    src_w, src_h = clip.width, clip.height
+
+    mask_format = clip.format.replace(color_family=vs.GRAY, subsampling_w=0, subsampling_h=0)
+
+    white = 1 if mask_format.sample_type == vs.FLOAT else (1 << bits) - 1
+
+    white_clip = core.std.BlankClip(clip, width, height, mask_format.id, 1, color=white, keep=True)
+
+    padded = white_clip.std.AddBorders(
+        offset_x, src_w - width - offset_x,
+        offset_y, src_h - height - offset_y
+    )
+
+    return padded * clip.num_frames
+
+
+@disallow_variable_format
+def replace_squaremask(
+    clipa: vs.VideoNode, clipb: vs.VideoNode, mask_params: Tuple[int, int, int, int],
+    ranges: Range | List[Range] | None = None,
+    blur_sigma: int | None = None, invert: bool = False
+) -> vs.VideoNode:
+    assert clipa.format
+    assert clipb.format
+
+    mask = squaremask(clipb, *mask_params)
+
+    if invert:
+        mask = mask.std.InvertMask()
+
+    if blur_sigma is not None:
+        if clipa.format.bits_per_sample == 32:
+            mask = mask.bilateralgpu.Bilateral(blur_sigma)
+        else:
+            mask = mask.bilateral.Gaussian(blur_sigma)
+
+    merge = clipa.std.MaskedMerge(clipb, mask)
+
+    return lvf.rfs(clipa, merge, ranges) if ranges else merge
+
+
+def freeze_replace_mask(
+    mask: vs.VideoNode, insert: vs.VideoNode,
+    mask_params: Tuple[int, int, int, int], frame: int, frame_range: Tuple[int, int]
+) -> vs.VideoNode:
+    masked_insert = replace_squaremask(mask[frame], insert[frame], mask_params)
+    return lvf.rfs(mask, masked_insert * mask.num_frames, frame_range)
