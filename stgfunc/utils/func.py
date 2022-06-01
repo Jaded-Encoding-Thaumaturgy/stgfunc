759a37b09c34da558d975d815e9d9e2f700d69fe
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-06-01 19:38:59 +0200

    Add cround, clamp, mod_x

--- stgfunc/utils/func.py
+++ stgfunc/utils/func.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from math import floor
+from math import floor, ceil
 from functools import partial, wraps
 from typing import Any, Callable, Iterable, List, Sequence, cast, overload
 
@@ -11,8 +11,40 @@ from ..types import F, T
 core = vs.core
 
 
-def mod4(x: float) -> int:
-    return 16 if x < 16 else floor(x / 4 + 0.5) * 4
+def cround(x: float) -> int:
+    return floor(x + 0.5) if x > 0 else ceil(x - 0.5)
+
+
+def clamp(val: float, min_val: float, max_val: float) -> float:
+    return min_val if val < min_val else max_val if val > max_val else val
+
+
+def mod_x(val: int | float, x: int) -> int:
+    return max(x * x, cround(val / x) * x)
+
+
+mod2 = partial(mod_x, x=2)
+
+mod4 = partial(mod_x, x=4)
+
+
+def to_arr(array: Sequence[T] | T) -> List[T]:
+    return list(
+        array if (type(array) in {list, tuple, range, zip, set, map, enumerate}) else [array]  # type: ignore
+    )
+
+
+def flatten(items: Iterable[T]) -> Iterable[T]:
+    for val in items:
+        if isinstance(val, Iterable) and not isinstance(val, (str, bytes)):
+            for sub_x in flatten(val):
+                yield sub_x
+        else:
+            yield val  # type: ignore
+
+
+def remove_chars(string: str, chars: str = '') -> str:
+    return string.translate({ord(char): None for char in chars})
 
 
 @overload
@@ -81,22 +113,3 @@ def disallow_variable_resolution(function: F | None = None, /, *, only_first: bo
         return function(*args, **kwargs)
 
     return cast(F, _check)
-
-
-def to_arr(array: Sequence[T] | T) -> List[T]:
-    return list(
-        array if (type(array) in {list, tuple, range, zip, set, map, enumerate}) else [array]  # type: ignore
-    )
-
-
-def flatten(items: Iterable[T]) -> Iterable[T]:
-    for val in items:
-        if isinstance(val, Iterable) and not isinstance(val, (str, bytes)):
-            for sub_x in flatten(val):
-                yield sub_x
-        else:
-            yield val  # type: ignore
-
-
-def remove_chars(string: str, chars: str = '') -> str:
-    return string.translate({ord(char): None for char in chars})
