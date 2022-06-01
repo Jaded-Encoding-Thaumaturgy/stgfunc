8fa4aa9a528900947ebb85e946f01d91594c2186
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-06-01 19:40:30 +0200

    Add new dehalo submodule

--- stgfunc/__init__.py
+++ stgfunc/__init__.py
@@ -1,8 +1,10 @@
 from . import (  # noqa: F401
-    deband, easing, exprfuncs, mask, misc, noise, other, oyster, traiNNing, transitions, tweaking, types, utils
+    deband, dehalo, easing, exprfuncs, mask, misc, noise,
+    other, oyster, traiNNing, transitions, tweaking, types, utils
 )
 
 from .deband import *  # noqa: F401, F403
+from .dehalo import *  # noqa: F401, F403
 from .easing import *  # noqa: F401, F403
 from .exprfuncs import *  # noqa: F401, F403
 from .mask import *  # noqa: F401, F403
