a552e406d7e7fb109c3529a65530cf5ccbc9b432
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-06-07 17:16:55 +0200

    Add new median_plane_value function

--- stgfunc/other.py
+++ stgfunc/other.py
@@ -8,10 +8,10 @@ from vsmask.edge import EdgeDetect, PrewittStd
 from vsmask.types import ensure_format as _ensure_format
 from vsutil import Dither
 from vsutil import Range as CRange
-from vsutil import depth, get_peak_value, scale_value
+from vsutil import depth, get_peak_value, scale_value, get_neutral_value
 
 from .types import T
-from .utils import cround, get_prop, pad_reflect
+from .utils import cround, get_prop, pad_reflect, get_planes
 
 core = vs.core
 
@@ -56,6 +56,75 @@ def bestframeselect(
         return clips[best]
 
     return core.std.FrameEval(clips[0], _select, diffs)
+
+
+def median_plane_value(
+    clip: vs.VideoNode, planes: int | Sequence[int] | None = None, single_out: bool = False, cuda: bool | None = None
+) -> vs.VideoNode:
+    import numpy as np
+
+    try:
+        import cupy  # type: ignore
+        cuda_available = True
+    except ImportError:
+        cupy = None
+        cuda_available = False
+
+    assert clip.format
+
+    do_cuda = cuda_available if cuda is None else cuda
+
+    npp = cupy if do_cuda and cuda_available and clip.height > 720 and clip.width > 1024 else np
+
+    norm_planes = get_planes(clip, planes)
+
+    if single_out:
+        def _median_pvalue_modify_frame(f: List[vs.VideoFrame], n: int) -> vs.VideoFrame:
+            fdst = f[1].copy()
+
+            max_val = 0
+
+            for plane in norm_planes:
+                farr = npp.asarray(f[0][plane])
+                farr = npp.reshape(farr, farr.shape[0] * farr.shape[1])
+
+                max_val = max(max_val, int(npp.bincount(farr).argmax()))
+
+            np.asarray(fdst[0])[0] = max_val
+
+            return fdst
+    else:
+        def _median_pvalue_modify_frame(f: List[vs.VideoFrame], n: int) -> vs.VideoFrame:
+            fdst = f[1].copy()
+
+            for plane in norm_planes:
+                farr = npp.asarray(f[0][plane])
+                farr = npp.reshape(farr, farr.shape[0] * farr.shape[1])
+
+                val = int(npp.bincount(farr).argmax())
+
+                np.asarray(fdst[plane])[0] = val
+
+            return fdst
+
+    if single_out:
+        out_format = vs.core.query_video_format(
+            vs.GRAY, clip.format.sample_type, clip.format.bits_per_sample
+        )
+    elif clip.format.color_family == vs.YUV:
+        out_format = clip.format.replace(subsampling_h=0, subsampling_w=0)
+    else:
+        out_format = clip.format
+
+    nluma, nchroma = get_neutral_value(clip), get_neutral_value(clip, True)
+
+    blankclip = clip.std.BlankClip(
+        1, 1, int(out_format), keep=True, color=[nluma, nchroma, nchroma][:out_format.num_planes]
+    )
+
+    outclip = blankclip.std.ModifyFrame([clip, blankclip], _median_pvalue_modify_frame)
+
+    return outclip.resize.Point(clip.width, clip.height)
 
 
 # Written by Vardë - https://gist.github.com/Setsugennoao/96b85d9d13e7a113e11557ec64d616a2
