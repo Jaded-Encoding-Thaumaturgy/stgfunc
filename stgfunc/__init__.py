ab8f41e14ed4e63912b01cd9c450d0eedbf4a978
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-11-14 16:43:01 +0100

    Remove `deband`, `noise`, `types`

--- stgfunc/__init__.py
+++ stgfunc/__init__.py
@@ -1,15 +1,9 @@
-from . import (  # noqa: F401
-    deband, easing, mask, misc, noise, other, oyster, traiNNing, transitions, tweaking, types, utils
-)
-from .deband import *  # noqa: F401, F403
+from . import easing, mask, misc, other, oyster, traiNNing, transitions, tweaking  # noqa: F401
 from .easing import *  # noqa: F401, F403
 from .mask import *  # noqa: F401, F403
 from .misc import *  # noqa: F401, F403
-from .noise import *  # noqa: F401, F403
 from .other import *  # noqa: F401, F403
 from .oyster import *  # noqa: F401, F403
 from .traiNNing import *  # noqa: F401, F403
 from .transitions import *  # noqa: F401, F403
 from .tweaking import *  # noqa: F401, F403
-from .types import *  # noqa: F401, F403
-from .utils import *  # noqa: F401, F403
--- stgfunc/mask.py
+++ stgfunc/mask.py
@@ -2,57 +2,28 @@ from __future__ import annotations
 
 import os
 from pathlib import Path
+from typing import NamedTuple
 
 from vsexprtools import ExprOp, combine
 from vstools import (
-    FrameRangeN, FrameRangesN, VSFunction, core, depth, disallow_variable_format, expect_bits, get_depth,
-    get_peak_value, get_y, insert_clip, iterate, vs, replace_ranges
+    FrameRangeN, FrameRangesN, VSFunction, disallow_variable_format, get_depth, insert_clip, replace_ranges, vs
 )
 
 from .misc import source as stgsource
-from .types import MaskCredit
 
 __all__ = [
-    'adg_mask',
     'perform_masks_credit',
     'to_gray', 'manual_masking', 'get_manual_mask',
     'tcanny', 'linemask',
-    'detail_mask',
     'squaremask', 'replace_squaremask',
     'freeze_replace_mask'
 ]
 
 
-@disallow_variable_format
-def adg_mask(clip: vs.VideoNode, luma_scaling: float = 8.0, relative: bool = False) -> vs.VideoNode:
-    """
-    Re-reimplementation of kageru's adaptive_grain mask as expr, with added `relative` param.
-    Really just the same speed and everything (though, *slightly* faster in float32),
-    it's just a plugin dep less in your *func if already make use of Akarin's plugin.
-    For a description of the math and the general idea, see his article. \n
-    https://blog.kageru.moe/legacy/adaptivegrain.html \n
-    https://git.kageru.moe/kageru/adaptivegrain
-    """
-    y = get_y(clip).std.PlaneStats(prop='P')
-
-    assert y.format
-
-    peak = get_peak_value(y)
-
-    is_integer = y.format.sample_type == vs.INTEGER
-
-    x_string, aft_int = (f'x {peak} / ', f' {peak} * 0.5 +') if is_integer else ('x ', '')
-
-    if relative:
-        x_string += 'Y! Y@ 0.5 < x.PMin 0 max 0.5 / log Y@ * x.PMax 1.0 min 0.5 / log Y@ * ? '
-
-    x_string += '0 0.999 clamp X!'
-
-    return y.akarin.Expr(
-        f'{x_string} 1 X@ X@ X@ X@ X@ '
-        '18.188 * 45.47 - * 36.624 + * 9.466 - * 1.124 + * - '
-        f'x.PAverage 2 pow {luma_scaling} * pow {aft_int}',
-    )
+class MaskCredit(NamedTuple):
+    mask: vs.VideoNode
+    start_frame: int
+    end_frame: int
 
 
 def perform_masks_credit(path: Path) -> list[MaskCredit]:
@@ -127,34 +98,6 @@ def linemask(clip_y: vs.VideoNode) -> vs.VideoNode:
         multi_detail_mask(clip_y, 0.011),
         multi_detail_mask(clip_y, 0.013)
     ], ExprOp.ADD)
-
-
-# Stolen from Light by yours truly <3
-def detail_mask(
-    clip: vs.VideoNode,
-    sigma: float = 1.0, rxsigma: list[int] = [50, 200, 350],
-    pf_sigma: float | None = 1.0, brz: tuple[int, int] = (2500, 4500),
-    rg_mode: int = 17
-) -> vs.VideoNode:
-    clip, bits = expect_bits(clip)
-
-    clip_y = get_y(clip)
-
-    pf = core.bilateral.Gaussian(clip_y, pf_sigma) if pf_sigma else clip_y
-    ret = pf.retinex.MSRCP(rxsigma, None, 0.005)
-
-    blur_ret = core.bilateral.Gaussian(ret, sigma)
-    blur_ret_diff = combine([blur_ret, ret], ExprOp.SUB).std.Deflate()
-    blur_ret_brz = iterate(blur_ret_diff, core.std.Inflate, 4)
-    blur_ret_brz = blur_ret_brz.std.Binarize(brz[0]).morpho.Close(size=8)
-
-    prewitt_mask = clip_y.std.Prewitt().std.Binarize(brz[1]).std.Deflate().std.Inflate()
-    prewitt_brz = prewitt_mask.std.Binarize(brz[1]).morpho.Close(size=4)
-
-    merged = combine([blur_ret_brz, prewitt_brz], ExprOp.ADD)
-    rm_grain = core.rgvs.RemoveGrain(merged, rg_mode)
-
-    return rm_grain if bits == 16 else depth(rm_grain, bits)
 
 
 @disallow_variable_format
