29270955da81b031b9711345cf9d071e222d6988
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-11-23 22:37:19 +0100

    Remove oyster

--- stgfunc/__init__.py
+++ stgfunc/__init__.py
@@ -1,9 +1,8 @@
-from . import easing, mask, misc, other, oyster, traiNNing, transitions, tweaking  # noqa: F401
+from . import easing, mask, misc, other, traiNNing, transitions, tweaking  # noqa: F401
 from .easing import *  # noqa: F401, F403
 from .mask import *  # noqa: F401, F403
 from .misc import *  # noqa: F401, F403
 from .other import *  # noqa: F401, F403
-from .oyster import *  # noqa: F401, F403
 from .traiNNing import *  # noqa: F401, F403
 from .transitions import *  # noqa: F401, F403
 from .tweaking import *  # noqa: F401, F403
