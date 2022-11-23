2c5582b8ad9a4c1951fa7a385cc25b6a7d8537f1
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-11-23 22:39:14 +0100

    Remove traiNNing submodule

--- stgfunc/__init__.py
+++ stgfunc/__init__.py
@@ -1,8 +1,7 @@
-from . import easing, mask, misc, other, traiNNing, transitions, tweaking  # noqa: F401
+from . import easing, mask, misc, other, transitions, tweaking  # noqa: F401
 from .easing import *  # noqa: F401, F403
 from .mask import *  # noqa: F401, F403
 from .misc import *  # noqa: F401, F403
 from .other import *  # noqa: F401, F403
-from .traiNNing import *  # noqa: F401, F403
 from .transitions import *  # noqa: F401, F403
 from .tweaking import *  # noqa: F401, F403
