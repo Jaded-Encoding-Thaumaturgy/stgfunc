6584206301e9b13073a256d214b932bbb663e07d
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-11-23 22:55:55 +0100

    Remove easing, transitions submodule

--- stgfunc/__init__.py
+++ stgfunc/__init__.py
@@ -1,7 +1,5 @@
-from . import easing, mask, misc, other, transitions, tweaking  # noqa: F401
-from .easing import *  # noqa: F401, F403
+from . import mask, misc, other, tweaking  # noqa: F401
 from .mask import *  # noqa: F401, F403
 from .misc import *  # noqa: F401, F403
 from .other import *  # noqa: F401, F403
-from .transitions import *  # noqa: F401, F403
 from .tweaking import *  # noqa: F401, F403
